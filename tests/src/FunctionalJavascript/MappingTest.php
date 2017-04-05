452291cde22d23c719b512e365c5db0d751fd8da
Christian Fritsch <chr.fritsch@gmx.net>
2017-04-05 07:19:26 +0200

    Travis integration (#1)
    * Travis integration
    
    * Add travis branch
    
    * Add phantomjs
    
    * Move phantomjs
    
    * Change path
    
    * Debug
    
    * Fix path
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Remove feature branch

---
+++ tests/src/FunctionalJavascript/MappingTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\Tests\thunder_print\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+
+/**
+ * Tests the mapping creation.
+ *
+ * @group thunder_print
+ */
+class MappingTest extends JavascriptTestBase {
+
+  /**
+   * Test Creation of a mapping.
+   */
+  public function testMappingCreation() {
+
+    $this->drupalGet('<front>');
+
+    $this->assertSession()->pageTextContains('Drupal');
+
+    $this->assertTrue(TRUE);
+  }
+
+}
