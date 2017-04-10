15faebd54e80a5b73c319c32fb485806984a92b4
Johannes Haseitl <johannes@undpaul.de>
2017-04-10 13:37:05 +0200

    TFP-1: Add TextFormattedLong type for tagmapping

---
+++ src/Plugin/TagMappingType/TextFormattedLong.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * TextPlain.php for tfp
+ */
+
+namespace Drupal\thunder_print\Plugin\TagMappingType;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\thunder_print\Annotation\TagMappingType;
+use Drupal\thunder_print\Plugin\TagMappingTypeBase;
+
+/**
+ * Class TextFormattedLong
+ * @package Drupal\thunder_print\Plugin\TagMappingType
+ * @TagMappingType(
+ *   id = "text_formatted_long",
+ *   label = @Translation("Text formatted (long)"),
+ * )
+ */
+class TextFormattedLong extends TagMappingTypeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    return [
+      'value' => [
+        'required' => TRUE,
+        'name' => $this->t('Value'),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function optionsForm(array $form, FormStateInterface $form_state) {
+    return $form;
+  }
+
+}
