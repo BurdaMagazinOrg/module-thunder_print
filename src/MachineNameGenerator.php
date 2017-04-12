529f8e48c8573f2aaca38e9ff3c7b928315dce68
Johannes Haseitl <johannes@undpaul.de>
2017-04-12 09:28:40 +0200

    TFP-1: Implements interface in MachineNameGenerator

--- src/MachineNameGenerator.php
+++ src/MachineNameGenerator.php
@@ -1,26 +1,89 @@
 <?php
 
 namespace Drupal\thunder_print;
-use Drupal\Core\Transliteration\PhpTransliteration;
+
+use Drupal\Component\Transliteration\TransliterationInterface;
+use Drupal\Component\Utility\Unicode;
 
 /**
- * Class MachineNameGenerator.
+ * Provides machine name generator.
  *
  * @package Drupal\thunder_print
  */
 class MachineNameGenerator implements MachineNameGeneratorInterface {
 
   /**
-   * Drupal\Core\Transliteration\PhpTransliteration definition.
+   * Transliteration service.
    *
-   * @var \Drupal\Core\Transliteration\PhpTransliteration
+   * @var \Drupal\Component\Transliteration\TransliterationInterface
    */
   protected $transliteration;
+
+  /**
+   * Callback to check for existing machine name.
+   *
+   * @var callable
+   */
+  protected $existsCallback;
+
   /**
    * Constructor.
    */
-  public function __construct(PhpTransliteration $transliteration) {
+  public function __construct(TransliterationInterface $transliteration) {
     $this->transliteration = $transliteration;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function generateMachineName($input) {
+    $language = \Drupal::languageManager()->getCurrentLanguage();
+
+    $transliterated = $this->transliteration->transliterate($input, $language->getId(), '_');
+    $transliterated = Unicode::strtolower($transliterated);
+    return $transliterated;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function generateUniqueMachineName($input) {
+    if (!$this->hasExistsCallback()) {
+      throw new \Exception('No existsCallback set for generating a unique machine name.');
+    }
+
+    $output = $this->generateMachineName($input);
+    $appendcount = 0;
+
+    do {
+
+      $return = call_user_func($this->existsCallback, ($appendcount) ? $output . '_'  . $appendcount: $output);
+      $appendcount++;
+
+    } while (!empty($return));
+
+    return $output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setExistsCallback(callable $callback) {
+    $this->existsCallback = $callback;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function unsetExistsCallback() {
+    unset($this->existsCallback);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function hasExistsCallback() {
+    return isset($this->existsCallback);
+  }
 }
