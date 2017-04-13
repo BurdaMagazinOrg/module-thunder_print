a76a4abd77fafcdcaad4991a56e2682e84961062
Johannes Haseitl <johannes@undpaul.de>
2017-04-13 07:04:29 +0200

    TFP-1 Fix machine name generation

--- src/MachineNameGenerator.php
+++ src/MachineNameGenerator.php
@@ -38,10 +38,13 @@ class MachineNameGenerator implements MachineNameGeneratorInterface {
    */
   public function generateMachineName($input) {
     $language = \Drupal::languageManager()->getCurrentLanguage();
-
-    $transliterated = $this->transliteration->transliterate($input, $language->getId(), '_');
-    $transliterated = Unicode::strtolower($transliterated);
-    return $transliterated;
+    $machine_name = $this->transliteration->transliterate($input, $language->getId(), '_');
+    $machine_name = Unicode::strtolower($machine_name);
+    // Only allow a-z, 0-9 or '_'.
+    $machine_name = preg_replace('/[^a-z0-9_]+/', '_', $machine_name);
+    // Replace multiple occurencies of '_' with a single '_'.
+    $machine_name = preg_replace('/_+/', '_', $machine_name);
+    return $machine_name;
   }
 
   /**
@@ -56,13 +59,12 @@ class MachineNameGenerator implements MachineNameGeneratorInterface {
     $appendcount = 0;
 
     do {
-
-      $return = call_user_func($this->existsCallback, ($appendcount) ? $output . '_'  . $appendcount: $output);
+      $unique = ($appendcount) ? $output . '_'  . $appendcount: $output;
+      $return = call_user_func($this->existsCallback, $unique);
       $appendcount++;
-
     } while (!empty($return));
 
-    return $output;
+    return $unique;
   }
 
   /**
