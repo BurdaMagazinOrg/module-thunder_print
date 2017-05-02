46d3290e023c3fdb0a9a1d296ea73dd8fd440b71
Christian Fritsch <chr.fritsch@gmx.net>
2017-05-02 10:13:50 +0200

    Replace disable/enable by toogle status

--- src/Controller/PrintArticleTypeController.php
+++ src/Controller/PrintArticleTypeController.php
@@ -17,30 +17,14 @@ class PrintArticleTypeController extends ControllerBase implements ContainerInje
   /**
    * {@inheritdoc}
    */
-  public function disable($print_article_type) {
+  public function toggleStatus($print_article_type) {
 
     /** @var \Drupal\thunder_print\Entity\PrintArticleTypeInterface $print_article_type */
     $print_article_type = $this->entityTypeManager()
       ->getStorage('print_article_type')
       ->load($print_article_type);
 
-    $print_article_type->disable()
-      ->save();
-
-    return $this->redirect('entity.print_article_type.collection');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function enable($print_article_type) {
-
-    /** @var \Drupal\thunder_print\Entity\PrintArticleTypeInterface $print_article_type */
-    $print_article_type = $this->entityTypeManager()
-      ->getStorage('print_article_type')
-      ->load($print_article_type);
-
-    $print_article_type->enable()
+    $print_article_type->setStatus(!$print_article_type->status())
       ->save();
 
     return $this->redirect('entity.print_article_type.collection');
