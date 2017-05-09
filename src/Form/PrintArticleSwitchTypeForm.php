63c8dbb8bdc0a103a0a5c1f2bf18e46a2c88162e
Christian Fritsch <chr.fritsch@gmx.net>
2017-05-09 13:11:21 +0200

    Compare field definitions instead of tags

--- src/Form/PrintArticleSwitchTypeForm.php
+++ src/Form/PrintArticleSwitchTypeForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\thunder_print\Form;
 
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -16,16 +17,32 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class PrintArticleSwitchTypeForm extends FormBase {
 
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
   protected $entityTypeManager;
+
+
+  /**
+   * The entity field manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
 
   /**
    * PrintArticleSwitchTypeForm constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   The entity type manager service.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entityFieldManager
+   *   The entity field manager service.
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, EntityFieldManagerInterface $entityFieldManager) {
     $this->entityTypeManager = $entityTypeManager;
+    $this->entityFieldManager = $entityFieldManager;
   }
 
   /**
@@ -33,7 +50,8 @@ class PrintArticleSwitchTypeForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager')
     );
   }
 
@@ -54,12 +72,14 @@ class PrintArticleSwitchTypeForm extends FormBase {
     $print_article = $this->entityTypeManager->getStorage('print_article')
       ->load($form_state->getValue('print_article'));
 
-    if ($print_article && $new_print_type) {
-      $cloned_node = $print_article->createDuplicate();
-      $cloned_node->type = $new_print_type;
-      $cloned_node->save();
+    $new_print_type = $this->entityTypeManager->getStorage('print_article_type')
+      ->load($new_print_type);
 
-      $form_state->setRedirectUrl($cloned_node->toUrl('edit-form'));
+    if ($print_article && $new_print_type) {
+      $print_article->type = $new_print_type->id();
+      $print_article->save();
+
+      $form_state->setRedirectUrl($print_article->toUrl('edit-form'));
     }
 
   }
@@ -74,12 +94,9 @@ class PrintArticleSwitchTypeForm extends FormBase {
 
     if ($print_article) {
 
-      /** @var \Drupal\thunder_print\Entity\PrintArticleTypeInterface $print_article_type */
-      $print_article_type = $this->entityTypeManager->getStorage('print_article_type')
-        ->load($print_article->bundle());
-
-      $currentTags = array_keys($print_article_type->getTags());
-      sort($currentTags);
+      $fieldDefinitions = $this->entityFieldManager->getFieldDefinitions('print_article', $print_article->bundle());
+      $fieldDefinitions = array_keys($fieldDefinitions);
+      sort($fieldDefinitions);
 
       $all_print_article_types = $this->entityTypeManager->getStorage('print_article_type')
         ->loadMultiple();
@@ -87,32 +104,39 @@ class PrintArticleSwitchTypeForm extends FormBase {
       $options = [];
       /** @var \Drupal\thunder_print\Entity\PrintArticleTypeInterface $entity */
       foreach ($all_print_article_types as $entity) {
-        $tags = array_keys($entity->getTags());
-        sort($tags);
-        if ($entity->id() != $print_article_type->id() && $currentTags == $tags) {
+        $bundleFieldDefinitions = $this->entityFieldManager->getFieldDefinitions('print_article', $entity->id());
+        $bundleFieldDefinitions = array_keys($bundleFieldDefinitions);
+        sort($bundleFieldDefinitions);
+
+        if ($entity->id() != $print_article->bundle() && empty(array_diff($fieldDefinitions, $bundleFieldDefinitions))) {
           $options[$entity->id()] = $entity->label();
         }
       }
 
-      $form['new_print_type'] = [
-        '#type' => 'select',
-        '#title' => $this->t('New snippet template'),
-        '#options' => $options,
-        '#required' => TRUE,
-      ];
+      if ($options) {
+        $form['new_print_type'] = [
+          '#type' => 'select',
+          '#title' => $this->t('New snippet template'),
+          '#options' => $options,
+          '#required' => TRUE,
+        ];
 
-      $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('Switch template'),
-      ];
-      $form['print_article'] = [
-        '#type' => 'hidden',
-        '#value' => $print_article->id(),
-      ];
+        $form['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Switch template'),
+        ];
+        $form['print_article'] = [
+          '#type' => 'hidden',
+          '#value' => $print_article->id(),
+        ];
+      }
+      else {
+        $form['error']['#markup'] = $this->t('No matching snippet templates.');
+      }
 
     }
     else {
-      $form['error']['#markup'] = 'No valid print article.';
+      $form['error']['#markup'] = $this->t('No valid print article.');
     }
 
     return $form;
